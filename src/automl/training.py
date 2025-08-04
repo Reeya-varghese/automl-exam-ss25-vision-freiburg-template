82279e34152a52573100d04385ee73f5f9da1648
ManasiJeste <manasi.jeste@gmail.com>
2025-08-04 21:51:38 +0200

    error in traning file solved

--- src/automl/training.py
+++ src/automl/training.py
@@ -11,7 +11,7 @@ import optuna
 from model import get_model, get_transforms
 from utils import calculate_mean_std
 from torch.utils.data import Subset, random_split
-from dac import DynamicAdjustmentController
+from dac import DynamicAlgorithmController
 from torchvision import transforms
 
 logger = logging.getLogger(__name__)
